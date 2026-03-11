3ddae5bef009fc1bfc560f62aaee843106037560
MatteoDePourcqUCLL <r1039758@ucll.be>
2026-03-11 21:54:33 +0100

    User delete endpoint

--- backend/src/main/java/be/ucll/repository/GoalRepository.java
+++ backend/src/main/java/be/ucll/repository/GoalRepository.java
@@ -3,5 +3,8 @@ package be.ucll.repository;
 import be.ucll.model.Goal;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface GoalRepository extends MongoRepository<Goal, String> {
+    List<Goal> findByUserId(String id);
 }
