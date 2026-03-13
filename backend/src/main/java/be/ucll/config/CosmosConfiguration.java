a76ba2b755d302ffbf13912fc0f1c28a488f109d
Jolan <jolan.hamelryck@student.ucll.be>
2026-03-13 09:48:02 +0100

    Hopefully the fix for everything

--- backend/src/main/java/be/ucll/config/CosmosConfiguration.java
+++ backend/src/main/java/be/ucll/config/CosmosConfiguration.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.azure.cosmos.CosmosClientBuilder;
-import com.azure.identity.DefaultAzureCredential;
-import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.spring.data.cosmos.config.AbstractCosmosConfiguration;
 import com.azure.spring.data.cosmos.repository.config.EnableCosmosRepositories;
 
@@ -18,15 +16,14 @@ public class CosmosConfiguration extends AbstractCosmosConfiguration {
     private String endpoint;
     @Value("${spring.cloud.azure.cosmos.database}")
     private String databaseName;
-    
+    @Value("${spring.cloud.azure.cosmos.key}")
+    private String key;
+
     @Bean
-    public CosmosClientBuilder getCosmosClientBuilder() {
-        DefaultAzureCredential credential = new DefaultAzureCredentialBuilder()
-            .build();
-        
+    public CosmosClientBuilder cosmosClientBuilder() {
         return new CosmosClientBuilder()
             .endpoint(endpoint)
-            .credential(credential);
+            .key(key);
     }
 
     @Override
