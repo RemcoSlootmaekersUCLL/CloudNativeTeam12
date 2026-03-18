25ef128439434a35d9498a91663dd834af642001
MatteoDePourcqUCLL <r1039758@ucll.be>
2026-03-18 15:54:05 +0100

    remove unused import

--- backend/src/main/java/be/ucll/controller/ExerciseController.java
+++ backend/src/main/java/be/ucll/controller/ExerciseController.java
@@ -2,7 +2,6 @@ package be.ucll.controller;
 
 import be.ucll.model.Exercise;
 import be.ucll.service.ExerciseService;
-import be.ucll.service.WorkoutService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
