0785c273ac5597be0f018363234e255ccf4ca477
RemcoSlootmaekersUCLL <r1035581@ucll.be>
2026-03-18 15:20:32 +0100

    revamped header styling

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="">
         <Header />
-        <main className="min-h-screen">{children}</main>
+        <main className="min-h-screen bg-green-100">{children}</main>
       </body>
     </html>
   );
--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -13,7 +13,7 @@ const Header: React.FC = () => {
   ];
 
   const linkClassname =
-    "mx-2 px-2 py-1 text-yellow-50 rounded-lg hover:bg-pink-200 hover:text-black transition-colors duration-300";
+    "mx-2 px-2 py-1 text-yellow-50 rounded-lg hover:bg-cyan-300/30 transition-colors duration-300";
 
   return (
     <header className="w-full bg-gradient-to-r from-green-600 to-emerald-600 py-1">
