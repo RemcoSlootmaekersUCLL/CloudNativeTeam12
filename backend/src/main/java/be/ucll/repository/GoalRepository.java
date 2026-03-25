f78287031934b3b177ecdf7b8375ff9069037763
MatteoDePourcqUCLL <r1039758@ucll.be>
2026-03-25 16:27:04 +0100

    small changes

--- backend/src/main/java/be/ucll/repository/GoalRepository.java
+++ backend/src/main/java/be/ucll/repository/GoalRepository.java
@@ -5,7 +5,11 @@ import com.azure.spring.data.cosmos.repository.CosmosRepository;
 import be.ucll.model.Goal;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GoalRepository extends CosmosRepository<Goal, String> {
     List<Goal> findByUserId(String id);
+
+    @Override
+    Optional<Goal> findById(String id);
 }
