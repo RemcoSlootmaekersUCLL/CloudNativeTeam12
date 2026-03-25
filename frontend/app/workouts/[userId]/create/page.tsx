bdbbf9873087b35dbb23deb71a4a8219e995e7af
RemcoSlootmaekersUCLL <r1035581@ucll.be>
2026-03-25 15:40:05 +0100

    changed width

--- frontend/app/workouts/[userId]/create/page.tsx
+++ frontend/app/workouts/[userId]/create/page.tsx
@@ -4,7 +4,7 @@ const createWorkoutPage: React.FC = () => {
   return (
     <>
       <div className="flex justify-center">
-        <div className="w-[30%] p-2 mt-4 bg-zinc-800 rounded-xl border border-gray-400">
+        <div className="w-[40%] p-2 mt-4 bg-zinc-800 rounded-xl border border-gray-400">
           <h1 className="font-xl font-bold text-2xl">New Workout</h1>
           <CreateWorkout />
         </div>
