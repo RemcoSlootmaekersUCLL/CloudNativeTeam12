466be3fba587d87aaba55e230a0ddd5cd429b6d4
RemcoSlootmaekersUCLL <r1035581@ucll.be>
2026-04-09 15:53:40 +0200

    removed useless main tag in profile page.tsx

--- frontend/app/profile/[userId]/page.tsx
+++ frontend/app/profile/[userId]/page.tsx
@@ -38,38 +38,36 @@ const UserPage: React.FC<Props> = async ({ params }: Props) => {
 
   return (
     <div>
-      <main>
-        <h1 className="text-center text-2xl font-bold p-4">Your Profile</h1>
-        {error && (
-          <div className="text-red-800" role="alert">
-            {error}
-          </div>
-        )}
-        {data && data.user != null && (
-          <>
-            <section className="table-container-style">
-              <ProfileOverview
-                user={data.user}
-                workouts={data.workouts}
-                goals={data.goals}
-              />
-            </section>
-          </>
-        )}
-        {data && data.user == null && (
-          <>
-            <section className="flex flex-col max-width-100 justify-center items-center px-4">
-              <h1>Please login first</h1>
-              <Link
-                href="/login"
-                className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              >
-                Go to login page
-              </Link>
-            </section>
-          </>
-        )}
-      </main>
+      <h1 className="text-center text-2xl font-bold p-4">Your Profile</h1>
+      {error && (
+        <div className="text-red-800" role="alert">
+          {error}
+        </div>
+      )}
+      {data && data.user != null && (
+        <>
+          <section className="table-container-style">
+            <ProfileOverview
+              user={data.user}
+              workouts={data.workouts}
+              goals={data.goals}
+            />
+          </section>
+        </>
+      )}
+      {data && data.user == null && (
+        <>
+          <section className="flex flex-col max-width-100 justify-center items-center px-4">
+            <h1>Please login first</h1>
+            <Link
+              href="/login"
+              className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Go to login page
+            </Link>
+          </section>
+        </>
+      )}
     </div>
   );
 };
