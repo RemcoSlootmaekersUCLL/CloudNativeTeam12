7edc6657130c9f269c9c39abab4afe03bc9658a1
MatteoDePourcqUCLL <r1039758@ucll.be>
2026-04-13 12:38:03 +0200

    exercise reps and stuff minimum checker added

--- backend/src/main/java/be/ucll/dto/WorkoutExerciseResponse.java
+++ backend/src/main/java/be/ucll/dto/WorkoutExerciseResponse.java
@@ -1,12 +1,13 @@
 package be.ucll.dto;
 
 import be.ucll.model.enums.Type;
+import jakarta.validation.constraints.Min;
 
 public record WorkoutExerciseResponse (String id,
                                        String name,
                                        Type type,
-                                       int reps,
-                                       int duration,
-                                       int caloriesBurned){
+                                       @Min(value = 1, message = "Reps must be bigger than 0") int reps,
+                                       @Min(value = 1, message = "Duration must be bigger than 0") int duration,
+                                       @Min(value = 1, message = "Calories burned must be bigger than 0") int caloriesBurned){
 
 }
