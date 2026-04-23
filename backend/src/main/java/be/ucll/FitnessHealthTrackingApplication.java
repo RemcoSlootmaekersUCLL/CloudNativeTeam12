bf8fed73f08ad43ceec3e0b60e5ec7f4cc51d9d0
Jolan Hamelryck <jolan.hamelryck@gmail.com>
2026-04-23 16:09:45 +0200

    Added caching to getAllWorkouts need to discuss getWorkoutByUserId caching

--- backend/src/main/java/be/ucll/FitnessHealthTrackingApplication.java
+++ backend/src/main/java/be/ucll/FitnessHealthTrackingApplication.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 import com.azure.spring.data.cosmos.repository.config.EnableCosmosRepositories;
 
 @SpringBootApplication(exclude = {
-    MongoAutoConfiguration.class,
-    MongoDataAutoConfiguration.class
+        MongoAutoConfiguration.class,
+        MongoDataAutoConfiguration.class
 })
 @EnableCosmosRepositories(basePackages = "be.ucll.repository")
+@EnableCaching
 public class FitnessHealthTrackingApplication {
     public static void main(String[] args) {
         SpringApplication.run(FitnessHealthTrackingApplication.class, args);
