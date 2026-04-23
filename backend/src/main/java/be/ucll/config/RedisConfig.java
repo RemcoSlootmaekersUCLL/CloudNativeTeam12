5a94e0189dbca65dc77f11d9dd81a07e6471b645
Jolan Hamelryck <jolan.hamelryck@gmail.com>
2026-04-23 18:36:56 +0200

    I lied i had to make one small change to make it work in dev but it does still work and you do still need the right properties files

--- backend/src/main/java/be/ucll/config/RedisConfig.java
+++ backend/src/main/java/be/ucll/config/RedisConfig.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -17,6 +18,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
+@Profile("prod")
 public class RedisConfig {
 
     @Bean
