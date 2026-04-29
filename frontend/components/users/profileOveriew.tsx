f43d420894a4e2422adf8c7129c8057865f69a62
RobbeVanBogaert <r0995052@ucll.be>
2026-04-29 15:47:48 +0200

    profile iets beter gemaakt

--- frontend/components/users/profileOveriew.tsx
+++ frontend/components/users/profileOveriew.tsx
@@ -12,8 +12,8 @@ const ProfileOverview: React.FC<Props> = ({ user, workouts, goals }) => {
     "justify-center text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center";
   return (
     <>
-      <div className="ml-4">
-        <h2 className="font-bold text-xl">User Information</h2>
+      <div className="flex flex-col items-center py-2">
+        <h2 className="font-bold text-xl py-2">User Information</h2>
         {user ? (
           <table className="userInfoTable">
             <tbody>
@@ -46,12 +46,12 @@ const ProfileOverview: React.FC<Props> = ({ user, workouts, goals }) => {
         )}
       </div>
 
-      <div className="mt-4 ml-4">
+      <div className="flex flex-col items-center py-2">
         <div className="flex gap-2 items-center mb-1">
-          <h2 className="font-bold text-lg">Your Workouts</h2>
-          <Link href={`/workouts/${user.id}/create`} className={createButton}>
-            Create Workout
-          </Link>
+          <h2 className="font-bold text-lg py-2">Your Workouts</h2>
+        </div>
+        <div className="pb-3">          
+          <Link href={`/workouts/${user.id}/create`} className={createButton}>Create Workout</Link>
         </div>
         {workouts ? (
           <table className="userWorkoutsTable">
@@ -96,12 +96,12 @@ const ProfileOverview: React.FC<Props> = ({ user, workouts, goals }) => {
         )}
       </div>
 
-      <div className="ml-4 mt-4">
+      <div className="flex flex-col items-center py-2">
         <div className="flex gap-2 items-center mb-1">
-          <h2 className="font-bold text-lg">Your Goals</h2>
-          <Link href={`/goals/create/${user.id}`} className={createButton}>
-            Create Goal
-          </Link>
+          <h2 className="font-bold text-lg py-2">Your Goals</h2>
+        </div>
+        <div className="pb-3">
+          <Link href={`/goals/create/${user.id}`} className={createButton}> Create Goal</Link>
         </div>
         {goals ? (
           <table className="userGoalsTable">
