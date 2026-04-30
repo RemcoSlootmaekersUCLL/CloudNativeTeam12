67df745f35ed5673e0ed7477cebfbaba3a0d0e8a
RemcoSlootmaekersUCLL <r1035581@ucll.be>
2026-04-30 18:25:16 +0200

    fixed cancel leading to goals page

--- frontend/components/exercises/createExercise.tsx
+++ frontend/components/exercises/createExercise.tsx
@@ -147,7 +147,7 @@ const CreateExercise: React.FC<Props> = ({ exercises }) => {
         </div>
         <div className="flex gap-x-1 mt-2">
           <Link
-            href={`/goals`}
+            href={`/exercises`}
             className="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >
             Cancel
