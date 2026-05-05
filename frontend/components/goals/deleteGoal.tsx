b8baac5448556b2f03412cca499e47bdbe3c3e8f
Jolan Hamelryck <jolan.hamelryck@gmail.com>
2026-05-05 21:27:31 +0200

    redirect from editing or creating goal is back to profile, styling change for profile overview

--- frontend/components/goals/deleteGoal.tsx
+++ frontend/components/goals/deleteGoal.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import goalService from "@/services/goalService";
 import { StatusMessage } from "@/types";
@@ -8,73 +8,75 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 type props = {
-    goalId: string;
-}
-
-const DeleteGoal: React.FC<props> = ({ goalId }) => {
-    const router = useRouter();
-
-
-    const [statusMessages, setStatusMessage] = useState<StatusMessage[]>([]);
-
-
-    const handleSubmit = async (event: { preventDefault: () => void }) => {
-        event.preventDefault();
-
-
-        setStatusMessage([])
-
-
-
-        try {
-            const response = await goalService.deleteGoal(goalId);
-            if (!response) {
-                console.log("API returned error");
-                return;
-            }
-            setStatusMessage([{ message: "Goal succesfully deleted.", type: "success" }])
-            setTimeout(() => {
-                router.push("/goals");
-            }, 1000);
-        } catch (error) {
-            setStatusMessage([{ message: (error as Error).message, type: "error" }])
-        }
-    };
-    return (
-        <div className="max-w-sm m-auto">
-            {statusMessages.length > 0 && (
-                <div className="row">
-                    <ul className="list-none mb-3 mx-auto text-center">
-                        {statusMessages.map(({ message, type }, index) => (
-                            <li key={index} className={classNames({
-                                'text-red-800': type === 'error',
-                                'text-green-800': type === 'success',
-                            })}>
-                                {message}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )}
-            <form onSubmit={handleSubmit}>
-                <div className="flex gap-x-1 mt-2 justify-center">
-                    <h1>Are you sure you want to delete goal {goalId}</h1>
-                </div>
-                <div className="flex gap-x-1 mt-2 justify-center">
-                    <Link href={`/goals`} className="text-white bg-gray-700 hover:bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                        Cancel
-                    </Link>
-                    <button
-                        className="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                        type="submit">
-                        Delete goal.
-                    </button>
-                </div>
-            </form>
-        </div>
-    );
-
+  goalId: string;
 };
 
+const DeleteGoal: React.FC<props> = ({ goalId }) => {
+  const router = useRouter();
+
+  const [statusMessages, setStatusMessage] = useState<StatusMessage[]>([]);
+
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+
+    setStatusMessage([]);
+
+    try {
+      const response = await goalService.deleteGoal(goalId);
+      if (!response) {
+        console.log("API returned error");
+        return;
+      }
+      setStatusMessage([
+        { message: "Goal succesfully deleted.", type: "success" },
+      ]);
+      setTimeout(() => {
+        router.push("/goals");
+      }, 1000);
+    } catch (error) {
+      setStatusMessage([{ message: (error as Error).message, type: "error" }]);
+    }
+  };
+  return (
+    <div className="max-w-sm m-auto">
+      {statusMessages.length > 0 && (
+        <div className="row">
+          <ul className="list-none mb-3 mx-auto text-center">
+            {statusMessages.map(({ message, type }, index) => (
+              <li
+                key={index}
+                className={classNames({
+                  "text-red-800": type === "error",
+                  "text-green-800": type === "success",
+                })}
+              >
+                {message}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <form onSubmit={handleSubmit}>
+        <div className="flex gap-x-1 mt-2 justify-center">
+          <h1>Are you sure you want to delete goal {goalId}</h1>
+        </div>
+        <div className="flex gap-x-1 mt-2 justify-center">
+          <Link
+            href={`/profile/${sessionStorage.getItem("id")}`}
+            className="text-white bg-gray-700 hover:bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            Cancel
+          </Link>
+          <button
+            className="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            type="submit"
+          >
+            Delete goal.
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default DeleteGoal;
